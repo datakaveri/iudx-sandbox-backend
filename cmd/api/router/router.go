a9eda2daca9cc95b746823b0bab5fd25d22cb61b
swarup-e <swarup@datakaveri.org>
2022-03-17 12:14:19 +0530

    Removed gallery APIs

--- cmd/api/router/router.go
+++ cmd/api/router/router.go
@@ -1,10 +1,6 @@
 package router
 
 import (
-	"github.com/iudx-sandbox-backend/cmd/api/handlers/gallery/createrepo"
-	"github.com/iudx-sandbox-backend/cmd/api/handlers/gallery/deleterepo"
-	"github.com/iudx-sandbox-backend/cmd/api/handlers/gallery/getrepo"
-	"github.com/iudx-sandbox-backend/cmd/api/handlers/gallery/listrepo"
 	"github.com/iudx-sandbox-backend/cmd/api/handlers/notebook/buildnotebook"
 	"github.com/iudx-sandbox-backend/cmd/api/handlers/notebook/deletenotebook"
 	"github.com/iudx-sandbox-backend/cmd/api/handlers/notebook/listnotebook"
@@ -17,11 +13,6 @@ import (
 
 func Get(app *application.Application) *httprouter.Router {
 	mux := httprouter.New()
-
-	mux.GET("/api/gallery", listrepo.Do(app))
-	mux.GET("/api/gallery/:id", getrepo.Do(app))
-	mux.POST("/api/gallery", createrepo.Do(app))
-	mux.DELETE("/api/gallery/:id", deleterepo.Do(app))
 
 	mux.GET("/api/notebooks", listnotebook.Do(app))
 	mux.GET("/api/notebooks/build-status", notebookbuildstatus.Do(app))
--- db/init_schema.sql
+++ db/init_schema.sql
@@ -1,13 +1,3 @@
-CREATE TABLE IF NOT EXISTS public.gallery
-(
-    "repoName"      TEXT,
-    "description"   TEXT,
-    "githubUrl"     TEXT            NOT NULL,
-    "createdAt"     TIMESTAMPTZ     NOT NULL    DEFAULT NOW(),
-    "updatedAt"     TIMESTAMPTZ     NOT NULL    DEFAULT NOW(),
-    PRIMARY KEY ("repoName")
-);
-
 CREATE TABLE IF NOT EXISTS public.notebook
 (
     "notebookId"    UUID            NOT NULL,
